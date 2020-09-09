269aadc7e1c8357e697573dcc6125dfa6fc0cc42
cain13 <abelkiyo@gmail.com>
2020-09-09 09:14:26 +0200

    [ABEL] - Sincronizo ccambio menu cerrar sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export class AppComponent {
   private textoCompartirAPP = 'Disfrute de la App de GrupoMPE para la gestión laboral, puede descargarla pinchando en el siguiente enlace!!';
   private urlCompartirAPP = 'http://onelink.to/ept9em';
 
-  public Version = 'Versión 1.0.1';
+  public Version = 'Versión 1.0.2';
   constructor(
     private platform: Platform,
     private menu: MenuController,
@@ -90,16 +90,16 @@ export class AppComponent {
         icon: 'clipboard-outline'
       },
       {
-        title: 'Formularios',
-        url: '/test',
-        direct: 'forward',
-        icon: 'school-outline'
-      },
-      {
         title: 'Citas Penditenes',
         url: '/citas-pendientes-trabajador',
         direct: 'forward',
         icon: 'timer-outline'
+      },
+      {
+        title: 'Formularios',
+        url: '/test',
+        direct: 'forward',
+        icon: 'school-outline'
       }
 
     ];
@@ -155,14 +155,14 @@ export class AppComponent {
         url: '/edit-profile',
         direct: 'forward',
         icon: 'person-outline'
-      },
+      } /*,
       {
         title: 'Cerrar Sesion',
         url: '/blanco',
         direct: 'forward',
         icon: 'power-outline'
       },
-      /* ,
+      ,
       {
         title: 'Centros MPE',
         url: '/property-list',
@@ -588,6 +588,13 @@ export class AppComponent {
     await actionSheet.present();
   }
 
+
+  cerrarSesion(){
+    console.log('Cerrar sesion');
+   // this.usuarioService.BorrarEmpleado();
+   // this.usuarioService.guardarUsuario(null);
+    this.navCtrl.navigateRoot('login');
+  }
   // goToEditProgile() {
   //   this.router.navigateByUrl('/edit-profile');
   // }
