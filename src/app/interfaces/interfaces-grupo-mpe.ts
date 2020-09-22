6b274605442342a531f28516224993c47878a6c0
cain13 <abelkiyo@gmail.com>
2020-09-22 12:15:24 +0200

    [ABEL] -> Inicio noticias con imagen

--- src/app/interfaces/interfaces-grupo-mpe.ts
+++ src/app/interfaces/interfaces-grupo-mpe.ts
@@ -730,6 +730,7 @@ export interface InfoCentrosMapa {
 
 export interface RespuestaAPINoticias {
   Respuesta: string;
+  ImagenDestacada: Noticia;
   Noticias:  Noticia[];
   Promocion: Noticia[];
   Codigo:    number;
